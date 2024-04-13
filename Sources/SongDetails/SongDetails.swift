573aebf32e8ca0d1eeecb32ec5416cd69e7d3f4c
mgsulaiman <info@mgsulaiman.com>
2024-04-13 13:45:37 +0300

    init

--- Sources/SongDetails/SongDetails.swift
+++ Sources/SongDetails/SongDetails.swift
@@ -1,2 +1,9 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import CommonModels
+import UIKit
+
+public protocol SongDetailsInterface {
+    func makeSongDetailsModule(navigationController: UINavigationController?, song: Song) -> UIViewController
+}
